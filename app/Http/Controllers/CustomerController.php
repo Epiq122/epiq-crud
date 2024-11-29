d56b3c36a358dea5154f678559a7c0a8ff101e0e
Robert Gleason <l2ob122@gmail.com>
2024-11-28 20:23:44 -0800

    created customer route, added create customer form, created customer controller

---
+++ app/Http/Controllers/CustomerController.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class CustomerController extends Controller
+{
+    public function index()
+    {
+        return view('customer.index');
+
+    }
+
+    public function create()
+    {
+        return view('customer.create');
+
+    }
+
+    public function store(Request $request)
+    {
+    }
+
+    public function show($id)
+    {
+    }
+
+    public function edit($id)
+    {
+    }
+
+    public function update(Request $request, $id)
+    {
+    }
+
+    public function destroy($id)
+    {
+    }
+}
