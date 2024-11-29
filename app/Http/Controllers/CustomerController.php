63cc31959dd8cf17d9dc1321fd0d81d8c801600d
Robert Gleason <l2ob122@gmail.com>
2024-11-28 21:42:47 -0800

    added logic to save data into the database

--- app/Http/Controllers/CustomerController.php
+++ app/Http/Controllers/CustomerController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Http\Requests\CustomerStoreRequest;
+use App\Models\Customer;
 use Illuminate\Http\Request;
 
 class CustomerController extends Controller
@@ -21,7 +22,17 @@ class CustomerController extends Controller
 
     public function store(CustomerStoreRequest $request)
     {
-        
+        $customer = new Customer();
+        $customer->first_name = $request->first_name;
+        $customer->last_name = $request->last_name;
+        $customer->email = $request->email;
+        $customer->phone = $request->phone;
+        $customer->bank_account_number = $request->bank_account_number;
+        $customer->about = $request->about;
+        $customer->save();
+
+        return redirect()->route('home');
+
     }
 
     public function show($id)
