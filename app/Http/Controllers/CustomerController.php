6855d1c4aa32889ce1c36e5f5a6c6db7da97a26f
Robert Gleason <l2ob122@gmail.com>
2024-11-30 12:53:48 -0800

    Add customer deletion functionality
    Implemented server-side deletion logic in CustomerController and integrated it with a corresponding delete action in the customer index view. This includes removing the customer image from the server and updating the UI to include a delete confirmation.

--- app/Http/Controllers/CustomerController.php
+++ app/Http/Controllers/CustomerController.php
@@ -88,5 +88,9 @@ class CustomerController extends Controller
 
     public function destroy($id)
     {
+        $customer = Customer::findOrFail($id);
+        File::delete(public_path($customer->image));
+        $customer->delete();
+        return redirect()->route('customers.index');
     }
 }
--- resources/views/customer/index.blade.php
+++ resources/views/customer/index.blade.php
@@ -61,11 +61,24 @@
                                     <a href="{{ route('customers.edit', $customer->id) }}" style="color: #2c2c2c;"
                                        class="ms-1 me-1"><i
                                             class="far fa-edit"></i></a>
+
                                     <a href="{{route('customers.show',$customer->id)}}" style="color: #2c2c2c;"
                                        class="ms-1 me-1"><i
                                             class="far fa-eye"></i></a>
-                                    <a href="" style="color: #2c2c2c;" class="ms-1 me-1"><i
+
+
+                                    <a href="javascript:;"
+                                       onclick="if(confirm('Are you sure you want to delete?'))
+                                       $('.form-{{$customer->id}}').submit()"
+                                       style="color: #2c2c2c;"
+                                       class="ms-1 me-1"><i
                                             class="fas fa-trash-alt"></i></a>
+                                    <form class="form-{{$customer->id}}"
+                                          action="{{route('customers.destroy',$customer->id)}}" method="POST">
+                                        @csrf
+                                        @method('DELETE')
+
+                                    </form>
                                 </td>
                         </tr>
                         @endforeach
